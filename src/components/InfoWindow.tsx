11d48d173d9c706d65e2b342924815119567d32a
Fredrik Malmo <fredrik@kodeworks.no>
2020-02-06 11:27:09 +0100

    Hydrate per render

--- src/components/InfoWindow.tsx
+++ src/components/InfoWindow.tsx
@@ -35,6 +35,9 @@ class InfoWindow extends React.Component<IInfoWindowProps> {
     this.infoWindow = new google.maps.InfoWindow({
       ...args.options,
       content: renderToString(
+        /**
+         * We use this div as the container into which we hydrate later.
+         */
         <div id={`infowindow-reactive-atlas-${this.token}`}>
           {this.props.children}
         </div>
@@ -47,7 +50,20 @@ class InfoWindow extends React.Component<IInfoWindowProps> {
       }
     });
 
-    google.maps.event.addListenerOnce(this.infoWindow, "domready", () => {
+    /**
+     * I had a lot of problems with this, so my end solution was to render and
+     * hydrate every time we try to rerender this component (detected when the
+     * google library reports that the content I told it to render is done
+     * rendering), which should essentially be every time new content is passed
+     * in (props.children). This is probably not the optimal solution, but it
+     * appears to work. When removing this, it won't pick up changes to the
+     * content.
+     *
+     * One sad thing about this though, is that as we're only hydrating when
+     * the dom is ready, we won't be able to updae this before it is rendered
+     * for the first time.
+     */
+    google.maps.event.addListener(this.infoWindow, "domready", () => {
       ReactDOM.hydrate(
         <>{this.props.children}</>,
         document.querySelector(`#infowindow-reactive-atlas-${this.token}`)
@@ -73,7 +89,32 @@ class InfoWindow extends React.Component<IInfoWindowProps> {
       this.infoWindow.close();
     }
 
+    /**
+     * Update the content rendered. This will also trigger the `domready`
+     * handler after a while, which will hydrate the content, and allow react
+     * to work with it.
+     */
+    this.infoWindow.setContent(
+      renderToString(
+        <div id={`infowindow-reactive-atlas-${this.token}`}>
+          {this.props.children}
+        </div>
+      )
+    );
+
     return null;
+  }
+
+  /**
+   * Remove the React rendered content from the Google InfoWindow.
+   */
+  public componentWillUnmount() {
+    const node: HTMLDivElement | null = document.querySelector(
+      `#infowindow-reactive-atlas-${this.token}`
+    );
+    if (node !== null) {
+      ReactDOM.unmountComponentAtNode(node);
+    }
   }
 }
 
